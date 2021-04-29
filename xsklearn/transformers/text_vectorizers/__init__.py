ead2fab9e714adb24df00bfb100d1637eba77bc5
altescy <altescy@fastmail.com>
2021-04-30 00:32:29 +0900

    add scdv

--- xsklearn/transformers/text_vectorizers/__init__.py
+++ xsklearn/transformers/text_vectorizers/__init__.py
@@ -1,6 +1,7 @@
 from xsklearn.transformers.text_vectorizers.bag_of_embeddings import (  # noqa: F401
     BagOfEmbeddings,
 )
+from xsklearn.transformers.text_vectorizers.scdv import SCDV  # noqa: F401
 from xsklearn.transformers.text_vectorizers.text_vectorizer import (  # noqa: F401
     TextVectorizer,
 )
